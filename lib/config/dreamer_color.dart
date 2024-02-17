eb78748e348abc15abcec9c271b48b183260752c
유지우 <treeyu12345@naver.com>
2024-02-17 17:00:33 +0900

    DreamerColor 추가

---
+++ lib/config/dreamer_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DreamerColor {
+  static Color keyPurple = const Color(0xFF9896F1);
+
+  static Color g1 = const Color(0xFF4B4E62);
+  static Color g2 = const Color(0xFFA6ABC0);
+  static Color g3 = const Color(0xFFD1D3DD);
+  static Color g4 = const Color(0xFFF1F2F5);
+}
