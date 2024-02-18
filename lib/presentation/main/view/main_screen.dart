6bdb291370878e872784cb193f327980bd6b4d54
iloveuhyeon <pyh1111802@naver.com>
2024-02-18 09:16:28 +0900

    매인 화면

--- lib/presentation/main/view/main_screen.dart
+++ lib/presentation/main/view/main_screen.dart
@@ -1,14 +1,35 @@
+import 'package:dreamer_flutter/config/dreamer_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 56,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35.0),
+              child: Text(
+                '꿈방',
+                style: TextStyle(
+                  color: DreamerColor.white,
+                  fontFamily: suit,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            Image.asset('assets/images/icons/main/main_library.png'),
+          ],
+        ),
       ),
     );
   }
